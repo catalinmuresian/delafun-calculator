000178f94ea31801b76191e4167e8f7b2f95e616
catalinmuresian <82952859+catalinmuresian@users.noreply.github.com>
2024-03-21 00:21:43 +0200

    first commit catalin

--- quasar.config.js
+++ quasar.config.js
@@ -23,7 +23,7 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-webpack/boot-files
     boot: [
-      
+
       'axios',
     ],
 
@@ -52,6 +52,7 @@ module.exports = configure(function (ctx) {
 
       // transpile: false,
       // publicPath: '/',
+      publicPath: process.env.NODE_ENV === 'production' && '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
@@ -69,9 +70,9 @@ module.exports = configure(function (ctx) {
 
       // https://v2.quasar.dev/quasar-cli-webpack/handling-webpack
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      
+
       chainWebpack (/* chain */) {}
-      
+
     },
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-devServer
@@ -118,9 +119,9 @@ module.exports = configure(function (ctx) {
       maxAge: 1000 * 60 * 60 * 24 * 30,
         // Tell browser when a file from the server should expire from cache (in ms)
 
-      
+
       chainWebpackWebserver (/* chain */) {},
-      
+
 
       middlewares: [
         ctx.prod ? 'compression' : '',
@@ -135,9 +136,9 @@ module.exports = configure(function (ctx) {
 
       // for the custom service worker ONLY (/src-pwa/custom-service-worker.[js|ts])
       // if using workbox in InjectManifest mode
-      
+
       chainWebpackCustomSW (/* chain */) {},
-      
+
 
       manifest: {
         name: `delafun-calculator`,
@@ -211,13 +212,13 @@ module.exports = configure(function (ctx) {
       },
 
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
-      
-      chainWebpackMain (/* chain */) {},
-      
 
-      
+      chainWebpackMain (/* chain */) {},
+
+
+
       chainWebpackPreload (/* chain */) {},
-      
+
     }
   }
 });
