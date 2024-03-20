b03b10809fd1f53eb499624a6c8350f54d1463ca
catalinmuresian <82952859+catalinmuresian@users.noreply.github.com>
2024-03-21 00:26:51 +0200

    build

--- quasar.config.js
+++ quasar.config.js
@@ -52,7 +52,9 @@ module.exports = configure(function (ctx) {
 
       // transpile: false,
       // publicPath: '/',
-      publicPath: process.env.NODE_ENV === 'production' && '/',
+      publicPath: process.env.NODE_ENV === 'production'
+        ? '/delafun-calculator/'
+        : '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
