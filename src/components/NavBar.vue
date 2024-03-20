2d609f35c4c269994a275dc5e2590399f78fb6b3
catalinmuresian <82952859+catalinmuresian@users.noreply.github.com>
2024-03-21 00:42:43 +0200

    deploy

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav-bar">
+    <div class="flex justify-between items-center bg-light-blue-3 q-px-lg q-py-sm">
+      <img
+        style="max-width: 100px;"
+        src="../assets/delafun__1_-removebg-preview.png"
+        alt="logo-delafun">
+      <span style="font-size: 26px;" class="text-white">Calculator</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+
+
+</style>
