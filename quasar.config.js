4f69a73a1c5509b0b525617d514c3a949607579d
catalinmuresian <82952859+catalinmuresian@users.noreply.github.com>
2024-03-28 14:18:49 +0200

    deploy

--- quasar.config.js
+++ quasar.config.js
@@ -148,8 +148,8 @@ module.exports = configure(function (ctx) {
         description: `An app calculator`,
         display: 'standalone',
         orientation: 'portrait',
-        background_color: '#97BEC9',
-        theme_color: '#97BEC9',
+        background_color: '#ACD0DB',
+        theme_color: '#ACD0DB',
         icons: [
           {
             "src": "android-chrome-192x192.png",
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="nav-bar">
-    <div class="flex justify-between items-center" style="padding: 5px 16px;background: #97BEC9">
+    <div class="flex justify-between items-center" style="padding: 5px 16px;background: #ACD0DB">
       <img
         style="max-width: 100px;"
         src="../assets/delafun__1_-removebg-preview.png"
