e2b7b38b08fe974701991ff3242c1b6f55bd6ca5
Catalin Muresian <catalinmuresian@yahoo.com>
2026-06-09 00:47:20 +0300

    updates

--- src-pwa/register-service-worker.js
+++ src-pwa/register-service-worker.js
@@ -1,41 +1,24 @@
 import { register } from 'register-service-worker'
 
-// The ready(), registered(), cached(), updatefound() and updated()
-// events passes a ServiceWorkerRegistration instance in their arguments.
-// ServiceWorkerRegistration: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
-
 register(process.env.SERVICE_WORKER_FILE, {
-  // The registrationOptions object will be passed as the second argument
-  // to ServiceWorkerContainer.register()
-  // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register#Parameter
+  ready (/* registration */) {},
+  registered (/* registration */) {},
+  cached (/* registration */) {},
+  updatefound (/* registration */) {},
 
-  // registrationOptions: { scope: './' },
-
-  ready (/* registration */) {
-    // console.log('Service worker is active.')
+  updated (registration) {
+    if (registration.waiting) {
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+    }
   },
 
-  registered (/* registration */) {
-    // console.log('Service worker has been registered.')
-  },
-
-  cached (/* registration */) {
-    // console.log('Content has been cached for offline use.')
-  },
-
-  updatefound (/* registration */) {
-    // console.log('New content is downloading.')
-  },
-
-  updated (/* registration */) {
-    // console.log('New content is available; please refresh.')
-  },
-
-  offline () {
-    // console.log('No internet connection found. App is running in offline mode.')
-  },
-
-  error (/* err */) {
-    // console.error('Error during service worker registration:', err)
-  }
+  offline () {},
+  error (/* err */) {}
 })
+
+// Reload the page when the new service worker takes control
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.addEventListener('controllerchange', () => {
+    window.location.reload()
+  })
+}
